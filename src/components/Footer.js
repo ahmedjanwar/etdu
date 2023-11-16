2c379a01b7daa7a10bc2133821f4944f818f2ab0
ahmedjanwar <ajemal840@gmail.com>
2023-11-17 00:12:40 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} ETdu. All rights reserved.
-        </p>
-        <div className="flex justify-center items-center space-x-4 mt-4">
-          {/* Social Media Icons */}
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="container mx-auto flex flex-col md:flex-row items-center md:items-start justify-between">
+        {/* Left Side: Company Description */}
+        <div className="mb-6 md:mb-0 text-center md:text-left md:w-1/2">
+          <p className="text-sm">
+            Follow us on social media for updates and news!
+          </p>
+          <div className="mt-4">
+            <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+          </div>
+        </div>
+
+        {/* Right Side: Social Links */}
+        <div className="flex justify-center items-center space-x-4 md:w-1/2">
           <a href="#" className="text-white hover:text-gray-500">
-            <i className="fab fa-facebook"></i>
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
           </a>
           <a href="#" className="text-white hover:text-gray-500">
-            <i className="fab fa-twitter"></i>
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
           <a href="#" className="text-white hover:text-gray-500">
-            <i className="fab fa-instagram"></i>
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
           </a>
           <a href="#" className="text-white hover:text-gray-500">
-            <i className="fab fa-linkedin"></i>
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
         </div>
+      </div>
+
+      {/* Bottom Center: All Rights Reserved */}
+      <div className="text-center mt-8">
+        <p className="text-xs">
+          &copy; {new Date().getFullYear()} ETdu. All rights reserved.
+        </p>
       </div>
     </footer>
   );
