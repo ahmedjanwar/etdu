5f9ed1e27a0740ef7516ad5ee091dffec4fe81ef
ahmedjanwar <ajemal840@gmail.com>
2023-11-17 00:24:46 +0200

    about us updated image to be changed

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -2,21 +2,34 @@ import React from 'react';
 
 const AboutUsSection = () => {
   return (
-    <section className="about-us bg-gray-200 py-16">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8">About Us</h2>
-        <p className="text-lg text-gray-700 mb-8">
-          ETdu is dedicated to guiding students on their journey to international education. Our mission is to empower individuals to achieve their dreams through access to quality education.
-        </p>
+    <section className="about-us bg-gray-100 py-16">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        {/* Left Side: Image */}
+        <div className="mb-8 md:mb-0 md:w-2/5">
+          <img
+            src={require('../images/AboutUs.jpg')} // Replace with the actual path to your image
+            alt="About Us"
+            className="rounded-md shadow-md"
+          />
+        </div>
 
-        <div className="flex justify-center">
-          <div className="w-1/2">
-            <img
-              src="https://via.placeholder.com/500"
-              alt="Team at ETdu"
-              className="rounded-lg shadow-md"
-            />
-          </div>
+        {/* Right Side: Text */}
+        <div className="md:w-1/2">
+          <h2 className="text-3xl font-bold mb-4">Who We Are</h2>
+          <p className="text-gray-700 mb-4">
+            ETdu is more than just an education consultancy. We are a dedicated team
+            passionate about guiding students on their journey to global education.
+          </p>
+          <p className="text-gray-700 mb-4">
+            Our mission is to empower students with the knowledge and resources
+            needed to succeed in a rapidly changing world. We believe in the
+            transformative power of education and strive to make the process
+            seamless for every aspiring student.
+          </p>
+          <p className="text-gray-700">
+            Whether you're dreaming of studying in Finland or any other part of
+            the world, ETdu is here to turn those dreams into reality.
+          </p>
         </div>
       </div>
     </section>
